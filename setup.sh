cc24679cb3b4c3114e00cd9591bc32472e86a77d
Arvind Krishna Srinivas <152021938+arvindkrishna0212@users.noreply.github.com>
2025-03-13 00:58:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pip install --upgrade pip
 pip install -r requirements.txt
+echo "I agree to the terms of the non-commercial CPML: https://coqui.ai/cpml" | pip install git+https://github.com/coqui-ai/TTS.git
